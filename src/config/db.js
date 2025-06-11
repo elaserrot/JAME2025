979259ad2ed8b74dbcf1f888418976f42473fbdb
leetosky27 <leetosky27@gmail.com>
2025-06-10 21:58:49 -0500

    config bd

--- src/config/db.js
+++ src/config/db.js
@@ -4,10 +4,10 @@ require('dotenv').config();
 
 const connectDB = async () => {
     const pool = mysql.createPool({
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASS,
-        database: process.env.DB_NAME,
+        host: process.env.DB_HOST || 'localhost',
+        user: process.env.DB_USER || 'root',
+        password: process.env.DB_PASS || '',
+        database: process.env.DB_NAME || 'jame',
         port: process.env.DB_PORT || 3306,
         connectTimeout: 10000,
         ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false
